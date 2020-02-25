84320fc410f858c6c971d42f33fe7e6a6f17c544
xujinshan361 <xujinshan361@163.com>
2020-02-25 21:57:43 +0800

    Spring 框架学习

---
+++ study_30_Spring09/src/com/xujinshan/service/UsersService.java
@@ -0,0 +1,9 @@
+package com.xujinshan.service;
+
+import com.xujinshan.pojo.Users;
+
+public interface UsersService {
+
+	int insert(Users users);
+	int insUsers(Users users);
+}
---
+++ study_30_Spring09/src/com/xujinshan/service/impl/UsersServiceImpl.java
@@ -0,0 +1,19 @@
+package com.xujinshan.service.impl;
+
+import com.xujinshan.pojo.Users;
+import com.xujinshan.service.UsersService;
+
+public class UsersServiceImpl implements UsersService{
+
+	@Override
+	public int insert(Users users) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int insUsers(Users users) {
+		insert(users);
+		return 0;
+	}
+}
+
